9643223de32822ff174efdee1f7a3a91a5a3b1a5
amous5902 <64476414+amous5902@users.noreply.github.com>
2025-12-19 14:43:11 +0530

    Disable SSL verification for InfinityFree proxy

--- events.php
+++ events.php
@@ -2,10 +2,17 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
-// Proxy InfinityFree DB API
 $url = "https://api.infiniaentertainment.in/api/db-events.php";
 
-$response = file_get_contents($url);
+$context = stream_context_create([
+    "ssl" => [
+        "verify_peer" => false,
+        "verify_peer_name" => false,
+        "allow_self_signed" => true
+    ]
+]);
+
+$response = file_get_contents($url, false, $context);
 
 if ($response === false) {
     http_response_code(500);
